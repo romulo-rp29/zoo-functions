8e54bfb52c86736e9315dde1e45558d9f808e2a9
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-02 17:46:17 -0300

    requisiro 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getEmployee = data.employees.filter((element) =>
+    element.id === id).map((element) =>
+    element.responsibleFor[0]);
+  const getOldestAnimal = data.species.find((element) =>
+    element.id === getEmployee[0]).residents.sort((element1, element2) =>
+    element2.age - element1.age).map((element) =>
+    [element.name, element.sex, element.age])[0];
+  return getOldestAnimal;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+//   const { species, hours } = data;
+//   const getAnimalsNames = species.filter((element) => element).map((element) => element.name);
+//   const getHours = hours;
+//   if (!scheduleTarget) {
+//     return
+//   }
 }
+// console.log(data.species.filter((element) => element).map((element) => element.name));
+// console.log(data.hours);
+// availabeDay: {
+//     officeHour: `Open from ${open} until ${close}`,
+//     exhibition: [ animal1, animal2, animal3, animal4 ],
+//   },
 
 module.exports = getSchedule;
