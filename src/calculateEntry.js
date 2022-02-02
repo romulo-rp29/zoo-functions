5be91deb70c6c8fc8cc44fd1fe1f989ca795fafc
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-02 10:45:35 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const ageCount = { child: 0, adult: 0, senior: 0 };
+  ageCount.child = entrants.filter((element) => element.age < 18).length;
+  ageCount.adult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+  ageCount.senior = entrants.filter((element) => element.age >= 50).length;
+  return ageCount;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || entrants.length === undefined) {
+    return 0;
+  }
+  const totalEntrants = countEntrants(entrants);
+  const pricesValue = data.prices;
+  const totalPrice = totalEntrants.child * pricesValue.child
+  + totalEntrants.adult * pricesValue.adult
+  + totalEntrants.senior * pricesValue.senior;
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
