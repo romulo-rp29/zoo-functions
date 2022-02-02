32be2d7db44ae31c585905502cf199b2b0a56169
romulo-rp29 <romul.ceuma@gmail.com>
2022-02-02 08:34:15 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const allAnimals = {};
+  if (!animal) {
+    data.species.forEach((element) => {
+      allAnimals[element.name] = element.residents.length;
+    });
+    return allAnimals;
+  }
+  const residentAnimals = data.species.find((specie) => specie.name === animal.specie).residents;
+  if (animal.specie && !animal.sex) {
+    return residentAnimals.length;
+  }
+  return residentAnimals.filter((element) => element.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
